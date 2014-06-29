484fc2373695bc597ea32f87fdd5fa0006eb9273
mpv <unarbolito@gmail.com>
2014-06-29 15:40:49 -0300

    added jump module
    Ported from oh-my-zsh jump plugin.

---
+++ modules/jump/README.md
@@ -0,0 +1,19 @@
+Jump
+====
+
+Jump around the filesystem by manually adding marks.
+
+Functions
+---------
+
+  - `mark` mark the current directory.
+  - `marks` lists marks.
+  - `jump` jump to a mark.
+  - `unmark` remove a mark.
+
+Aliases
+-------
+
+  - `j` jump to a mark
+  - `m` mark the current directory
+
---
+++ modules/jump/init.zsh
@@ -0,0 +1,41 @@
+# Easily jump around the file system by manually adding marks
+# marks are stored as symbolic links in the directory $MARKPATH (default $HOME/.marks)
+#
+# jump FOO: jump to a mark named FOO
+# mark FOO: create a mark named FOO
+# unmark FOO: delete a mark
+# marks: lists all marks
+#
+export MARKPATH=$HOME/.marks
+
+#
+# Aliases
+#
+
+alias j='jump'
+alias m='mark'
+
+
+#
+# Completion
+#
+
+_completemarks() {
+	if [[ $(ls "${MARKPATH}" | wc -l) -gt 1 ]]; then
+		reply=($(ls $MARKPATH/**/*(-) | grep : | sed -E 's/(.*)\/([_\da-zA-Z\-]*):$/\2/g'))
+	else
+		if readlink -e "${MARKPATH}"/* &>/dev/null; then
+			reply=($(ls "${MARKPATH}"))
+		fi
+	fi
+}
+compctl -K _completemarks jump
+compctl -K _completemarks unmark
+
+_mark_expansion() {
+	setopt extendedglob
+	autoload -U modify-current-argument
+	modify-current-argument '$(readlink "$MARKPATH/$ARG")'
+}
+zle -N _mark_expansion
+bindkey "^g" _mark_expansion
