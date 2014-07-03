3fa3692ddb0915a151e27beba423d583dfaa26af
mpv <unarbolito@gmail.com>
2014-07-02 21:29:22 -0300

    added module for drupal development

---
+++ modules/drupal/init.zsh
@@ -0,0 +1,20 @@
+# Drupal related aliases.
+
+#
+# Aliases
+#
+
+alias cdd='cd_drupal_dir'
+
+#
+# Drush location.
+#
+drush_command=$(which drush)
+drush_dir=$drush_command:A:h
+
+#
+# Enable drush's default bash autocomplete script.
+#
+autoload bashcompinit
+bashcompinit
+source $drush_dir/drush.complete.sh
