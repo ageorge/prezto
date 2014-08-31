c894d6c2858ceceb3a12d49e969df982d7f8a729
ariel <ariel@gcoop.coop>
2014-08-31 19:04:12 -0300

    check for drush before doing anything

--- modules/drupal/init.zsh
+++ modules/drupal/init.zsh
@@ -6,15 +6,17 @@
 
 alias cdd='cd_drupal_dir'
 
-#
-# Drush location.
-#
-drush_command=$(which drush)
-drush_dir=$drush_command:A:h
-
-#
-# Enable drush's default bash autocomplete script.
-#
-autoload bashcompinit
-bashcompinit
-source $drush_dir/drush.complete.sh
+if type drush > /dev/null; then
+    #
+    # Drush location.
+    #
+    drush_command=$(which drush)
+    drush_dir=$drush_command:A:h
+    
+    #
+    # Enable drush's default bash autocomplete script.
+    #
+    autoload bashcompinit
+    bashcompinit
+    source $drush_dir/drush.complete.sh
+fi
